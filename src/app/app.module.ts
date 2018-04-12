30615d5f3aeb4da1de3ff5ba538b3977f0d4562e
HARRISON\harrison <harrison.croaker@hotmail.com>
2018-04-12 18:41:36 +1000

    Initial Push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { SearchPage } from '../pages/search/search';
+import { SearchProvider } from '../providers/search/search';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    SearchPage
   ],
   imports: [
     BrowserModule,
@@ -19,12 +20,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    SearchPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SearchProvider
   ]
 })
 export class AppModule {}
